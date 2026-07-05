d2c285be029faa978f85a681337f31baa153c372
knpluitstudents <kodingnextpluitstudents@gmail.com>
2026-07-05 14:10:50 +0700

    rttr

--- hospital_app (1).py
+++ hospital_app (1).py
@@ -125,6 +125,24 @@ with st.form("triage_form"):
     </div>
     """, unsafe_allow_html=True)
 
+    c1, c2, c3, c4 = st.column(4)
+    with c1:  
+        fever = st.checkbox("Fever")
+        cough = st.checkbox("Cough")
+    with c2:  
+        headache = st.checkbox("Headache")
+        chest_pain = st.checkbox("Chest Pain")
+    with c3:  
+        stomach_pain = st.checkbox("Stomach Pain")
+        shortness_breath = st.checkbox("Shortness of Breath")
+    with c4:  
+        nausea = st.checkbox("Nausea")
+        dizziness = st.checkbox("Dizziness")
+    c5, _, _, _ = st.columns(4)
+    with c5:
+        skin_rash = st.checkbox("Skin Rash")
+    
+
     # TODO (logic): create 4 columns (st.columns(4)) with 9 symptom checkboxes.
     # The model needs THESE EXACT variable names (boolean type from st.checkbox):
     #   fever, cough, headache, chest_pain, stomach_pain,
