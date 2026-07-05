2d5768736102ddd9fdcc7f5b0a388ccd12e98e99
knpluitstudents <kodingnextpluitstudents@gmail.com>
2026-07-05 14:26:58 +0700

    gr

--- hospital_app (1).py
+++ hospital_app (1).py
@@ -173,7 +173,7 @@ with st.form("triage_form"):
     </div>
     """, unsafe_allow_html=True)
 
-    col_cc, col_dur = st,columns(2)
+    col_cc, col_dur = st.columns(2)
     with col_cc:
         chief_complaint = st.selectbox("Chief Complaint",options=list(cc_map.keys()))
     with col_dur:
