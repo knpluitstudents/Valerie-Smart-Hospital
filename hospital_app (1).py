01ca0eaa62b93c839702893c03d0fc9c17835ff5
knpluitstudents <kodingnextpluitstudents@gmail.com>
2026-07-05 13:53:00 +0700

    u

--- hospital_app (1).py
+++ hospital_app (1).py
@@ -114,10 +114,10 @@ with st.form("triage_form"):
     # TODO (text/color): replace CODENO4 and COLOR2.
     st.markdown("""
     <!-- COLOR2 -->
-    <div style="background:#7cb1ff;border:1px solid #ffffff;border-radius:14px;
+    <div style="background:#7db1ff;border:1px solid #ffffff;border-radius:14px;
                 padding:20px 24px;margin-bottom:20px;">
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
-            <span style="background:#45aeff;color:white;border-radius:8px;
+            <span style="background:#8dbbff;color:white;border-radius:8px;
                          padding:4px 10px;font-size:12px;font-weight:600;">1</span>
             <span style="font-size:16px;font-weight:600;color:#ffffff;">What are your main symptoms?</span>
             <span style="font-size:13px;color:#ffffff;font-style:italic;">select all that apply</span>
